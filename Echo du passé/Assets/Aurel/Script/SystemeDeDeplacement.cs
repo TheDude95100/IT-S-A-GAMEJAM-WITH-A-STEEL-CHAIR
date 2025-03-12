b7395592f8ad99f8418a0c8aa1861bf63bd6864e
aylu <aylumination@gmail.com>
2025-03-12 18:27:14 -0400

    ajout du character controller

--- Echo du passé/Assets/Aurel/Script/SystemeDeDeplacement.cs
+++ Echo du passé/Assets/Aurel/Script/SystemeDeDeplacement.cs
@@ -1,16 +1,27 @@
+using System;
+using JetBrains.Annotations;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SystemeDeDeplacement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    InputAction moveAction;
+    public float Speed = 5f;
+
+    private void Start()
     {
         
+        moveAction = InputSystem.actions.FindAction("Move");
     }
-
-    // Update is called once per frame
     void Update()
     {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>(); //format float(X,Y)  ou X va gérer notre déplacement 'latéral' (axe X) et Y le deplacement 'horizontal' (axe Z)
+        Debug.Log(moveValue);
         
+        Vector3 moveDirection = new Vector3(moveValue.x, 0, moveValue.y);
+        transform.Translate(moveDirection * Speed * Time.deltaTime);
+
+
     }
 }
