e11352ff9885130dc1e8ab98fee8bcdb768855af
igatia <war_mai@hotmail.com>
2025-03-12 23:39:16 -0400

    Win-lose Condition

--- Echo du passé/Assets/Warren/Manager.cs
+++ Echo du passé/Assets/Warren/Manager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Combat
@@ -6,17 +8,33 @@ namespace Combat
     {
         private Entity[] entityList;
 
+        private List<int> enemyIndexes,
+                          allyIndexes;
+
+        private int selected;
+
         private void Awake()
         {
             entityList = FindObjectsByType<Entity>(FindObjectsSortMode.None);
+            enemyIndexes = new List<int>();
+            allyIndexes = new List<int>();
         }
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            foreach(Entity entity in entityList)
+            for(int counter = 0; counter < entityList.Length; counter++)
             {
-                Debug.Log(entity.gameObject.name);
+                Debug.Log(entityList[counter].gameObject.name);
+
+                if(entityList[counter].CompareTag("Player"))
+                {
+                    allyIndexes.Add(counter);
+                }
+                else
+                {
+                    enemyIndexes.Add(counter);
+                }
             }
         }
 
@@ -26,10 +44,85 @@ namespace Combat
 
         }
 
+        private void FixedUpdate()
+        {
+            if(VerifyWin())
+            {
+                Debug.Log("You win");
+            }
+            if (VerifyLose())
+            {
+                Debug.Log("You lost");
+            }
+        }
+
         public void OnAction()
         {
             Debug.Log("Action taken");
-            entityList[0].TakeDamage(1);
+            entityList[enemyIndexes[selected]].TakeDamage(1);
+        }
+
+        public void OnSelectionRight()
+        {
+            selected++;
+            if(selected >= enemyIndexes.Count)
+            {
+                ResetSelection();
+            }
+            //Debug.Log(entityList[enemyIndexes[selected]].gameObject.name + " selected");
+        }
+
+        public void OnSelectionLeft()
+        {
+            selected--;
+            if (selected < 0)
+            {
+                selected = enemyIndexes.Count - 1;
+            }
+            //Debug.Log(entityList[enemyIndexes[selected]].gameObject.name + " selected");
+        }
+
+        public void ResetSelection()
+        {
+            selected = 0;
+        }
+
+        private bool VerifyWin()
+        {
+            int nbDeath = 0;
+            foreach(int index in enemyIndexes)
+            {
+                if (!entityList[index].IsAlive)
+                {
+                    nbDeath++;
+                }
+            }
+
+            if(nbDeath == enemyIndexes.Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool VerifyLose()
+        {
+            int nbDeath = 0;
+            foreach (int index in allyIndexes)
+            {
+                if (!entityList[index].IsAlive)
+                {
+                    nbDeath++;
+                }
+            }
+
+            if (nbDeath == allyIndexes.Count)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
