59fc0a77f98c46f7a99b1b6e8f8804bcba379330
2001085 <hcalvet70@gmail.com>
2025-03-12 23:22:34 -0400

    Oeeee les stats pendant le fight

---
+++ Echo du passé/Assets/Hugo/CharacterStatus.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharacterStatus : MonoBehaviour
+{
+    [SerializeField] CombatManager.Character character;
+    [SerializeField] TextMeshProUGUI _hpValue, _mpValue, _characterName;
+    [SerializeField] Slider _hpSlider, _mpSlider;
+
+    public TextMeshProUGUI HpValue { get => _hpValue; set => _hpValue = value; }
+    public TextMeshProUGUI MpValue { get => _mpValue; set => _mpValue = value; }
+    public TextMeshProUGUI CharacterName { get => _characterName; set => _characterName = value; }
+    public Slider HpSlider { get => _hpSlider; set => _hpSlider = value; }
+    public Slider MpSlider { get => _mpSlider; set => _mpSlider = value; }
+    public CombatManager.Character Character { get => character; set => character = value; }
+}
---
+++ Echo du passé/Assets/Hugo/PlayerStatus.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    [SerializeField] private GameObject _characterStatusPrefab;
+    private CombatManager _combatManager;
+    private int _numberOfCharacters;
+    private int _Y_spacing = -100;
+    private int _X_position = -120;
+    private int _Y_position = -50;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _combatManager = FindFirstObjectByType<CombatManager>();
+        _combatManager.OnInitiativeRolled += CombatManager_OnInitiativeRolled;
+    }
+    
+    private void CombatManager_OnInitiativeRolled(object sender, System.EventArgs e)
+    {
+        CreateVisual();
+    }
+
+    private void CreateVisual()
+    {
+        _numberOfCharacters = _combatManager.CurrentNumberOfCharacters;
+        for (int i = 0; i < _numberOfCharacters; i++)
+        {
+            // Instantie le prefab pour chaque personnage
+            GameObject go = Instantiate(_characterStatusPrefab, transform.localPosition, Quaternion.identity, transform);
+            go.transform.localPosition = new Vector3(_X_position, _Y_position + i * _Y_spacing, 0);
+            go.name = "Character" + i;  // Donne un nom unique à chaque personnage
+            CharacterStatus displayedStatsHolder = go.GetComponent<CharacterStatus>();
+            displayedStatsHolder.CharacterName.text = _combatManager.GetCharacter(i).Name;
+        }
+    }
+}
