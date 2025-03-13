3b91628cbbc578cf0e117bc10e5ba7f37a6474e4
BenjaminRodot <78915615+BenjaminRodot@users.noreply.github.com>
2025-03-13 12:15:41 -0400

    Editor debug

--- Echo du passé/Assets/Scripts/Data/JobData.cs
+++ Echo du passé/Assets/Scripts/Data/JobData.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class JobData : ScriptableObject
 {
     [SerializeField]
-    private string jobName = "...";
+    private string _jobName = "...";
 
     [SerializeField] 
     private SkillData[] _skillList;
     [SerializeField] 
     private Archetype _archetype = Archetype.None;
 
-    public string JobName => jobName;
+    public string JobName => _jobName;
     public SkillData[] SkillList => _skillList;
     public Archetype Archetype => _archetype;
 
