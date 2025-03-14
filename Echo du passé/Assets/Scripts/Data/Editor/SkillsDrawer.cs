8479aabd598bce6cdc4ffb05bddc562bdebb4ca0
BenjaminRodot <78915615+BenjaminRodot@users.noreply.github.com>
2025-03-14 11:25:36 -0400

    Editor Skill

--- Echo du passé/Assets/Scripts/Data/Editor/SkillsDrawer.cs
+++ Echo du passé/Assets/Scripts/Data/Editor/SkillsDrawer.cs
@@ -15,18 +15,19 @@ public class SkillsDrawer : PropertyDrawer
     private SerializedProperty _healing;
     private SerializedProperty _buffs;
 
+    private int parametresCount = 14;
     private int padding = 5;
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         EditorGUI.BeginProperty(position, label, property);
 
-        _skillName = property.FindPropertyRelative("_skillName");
-        _skillSlot = property.FindPropertyRelative("_skillSlot");
-        _description = property.FindPropertyRelative("_description");
-        _element = property.FindPropertyRelative("_element");
-        _focus = property.FindPropertyRelative("_focus");
-        _damage = property.FindPropertyRelative("_damage");
+        _skillName = property.FindPropertyRelative("_skillName");//
+        _skillSlot = property.FindPropertyRelative("_skillSlot");//
+        _description = property.FindPropertyRelative("_description");//
+        _element = property.FindPropertyRelative("_element"); //
+        _focus = property.FindPropertyRelative("_focus");//
+        _damage = property.FindPropertyRelative("_damage");//
         _accuracy = property.FindPropertyRelative("_accuracy");
         _curses = property.FindPropertyRelative("_curses");
         _healing = property.FindPropertyRelative("_healing");
@@ -37,17 +38,25 @@ public class SkillsDrawer : PropertyDrawer
 
         if (property.isExpanded)
         {
-            DrawNameProperty(position);
-            DrawArchetypeProperty(position);
+            DrawNameProperty(position,1);
+            DrawElementProperty(position, 2);
+            DrawDescriptionProperty(position, 3);
+            DrawSkillSlotProperty(position, 4);
+            DrawFocusProperty(position, 5);
+            DrawAccuracyProperty(position, 6);
+            DrawDamageProperty(position, 7);
+            DrawCurseProperty(position, 8);
+            DrawHealingProperty(position, 11);
+            DrawBuffProperty(position, 12);
         }
 
         EditorGUI.EndProperty();
 
     }
-    private void DrawNameProperty(Rect position)
+    private void DrawNameProperty(Rect position,int lineNumber)
     {
         float xPos = position.min.x;
-        float yPos = position.min.y + EditorGUIUtility.singleLineHeight;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber - 1);
         float width = position.size.x;
         float height = EditorGUIUtility.singleLineHeight;
 
@@ -55,15 +64,103 @@ public class SkillsDrawer : PropertyDrawer
         EditorGUI.PropertyField(drawArea, _skillName, new GUIContent("Name"));
     }
 
-    private void DrawArchetypeProperty(Rect position)
+    private void DrawElementProperty(Rect position, int lineNumber)
     {
         float xPos = position.min.x;
-        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * 2 + padding;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber -1);
         float width = position.size.x;
         float height = EditorGUIUtility.singleLineHeight;
 
         Rect drawArea = new Rect(xPos, yPos, width, height);
         EditorGUI.PropertyField(drawArea, _element, new GUIContent("Element"));
+    }
+
+    private void DrawDescriptionProperty(Rect position, int lineNumber)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber - 1);
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawArea, _description, new GUIContent("Description"));
+    }
+
+    private void DrawSkillSlotProperty(Rect position, int lineNumber)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber - 1);
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawArea, _skillSlot, new GUIContent("SkillSlot"));
+    }
+
+    private void DrawDamageProperty(Rect position, int lineNumber)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber - 1);
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawArea, _damage, new GUIContent("Damage"));
+    }
+
+    private void DrawFocusProperty(Rect position, int lineNumber)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber - 1);
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawArea, _focus, new GUIContent("Focus"));
+    }
+
+    private void DrawAccuracyProperty(Rect position, int lineNumber)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber - 1);
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawArea, _accuracy, new GUIContent("Accuracy"));
+    }
+
+    private void DrawHealingProperty(Rect position, int lineNumber)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber - 1);
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawArea, _healing, new GUIContent("Healing"));
+    }
+
+    private void DrawBuffProperty(Rect position, int lineNumber)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber - 1);
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawArea, _buffs, new GUIContent("Buff"));
+    }
+
+    private void DrawCurseProperty(Rect position, int lineNumber)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight * lineNumber + padding * (lineNumber - 1);
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawArea = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawArea, _curses, new GUIContent("Curse"));
     }
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
@@ -72,7 +169,7 @@ public class SkillsDrawer : PropertyDrawer
 
         if (property.isExpanded)
         {
-            totalLines += 3;
+            totalLines += parametresCount*2- parametresCount/2 - 2;
         }
 
         return (EditorGUIUtility.singleLineHeight * totalLines);
