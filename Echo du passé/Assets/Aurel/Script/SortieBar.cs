2b055b1064eba5ca359d8fa52ac69b10771d218e
aylu <aylumination@gmail.com>
2025-03-14 10:41:13 -0400

    bar fini a moitié

--- Echo du passé/Assets/Aurel/Script/SortieBar.cs
+++ Echo du passé/Assets/Aurel/Script/SortieBar.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Bar : InteractionSystem
+public class SortieBar : InteractionSystem
 {
     protected override void Interact()
     {
         Debug.Log(gameObject.name);
-        SceneManager.LoadScene("Bar");
+        SceneManager.LoadScene("Village");
     }
 }
